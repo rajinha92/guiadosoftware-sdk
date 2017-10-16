74a9ce409de3f5fb4a09b69c07e7b8f8abc8cde8
rafael <rafael.conrado.farias@gmail.com>
2017-10-16 14:00:10 -0200

    fix subcategory_attributes

--- src/Domains/SubCategory.php
+++ src/Domains/SubCategory.php
@@ -198,6 +198,6 @@ class SubCategory extends Domain
 		$this->updated_at       = $this->getSafeDateTime('updated_at', $source);
 		$this->content          = $this->getSafe('content', $source);
 		$this->title            = $this->getSafe('title', $source);
-		$this->subcategory_attributes = $this->getSafe('subcategory_attributes');
+		$this->subcategory_attributes = $this->getSafe('subcategory_attributes', $source);
 	}
 }
--- src/Http/Resources/SubCategory.php
+++ src/Http/Resources/SubCategory.php
@@ -8,53 +8,54 @@
 
 namespace GuiaDoSoftwareSdk\Http\Resources;
 
-
 use GuiaDoSoftwareSdk\Http\Requester;
 use GuiaDoSoftwareSdk\Domains\SubCategory as SubCategoryDomain;
 
 class SubCategory extends Requester
 {
-    public function get($id)
-    {
-        $response = $this->request('GET', '/sub-category/' . $id, []);
+	public function get($id)
+	{
+		$response = $this->request('GET', '/sub-category/' . $id, []);
 
-        return new SubCategoryDomain($response);
-    }
+		return new SubCategoryDomain($response);
+	}
 
-    public function bootgrid($start, $length, $search, array $order)
-    {
-        return $this->request('GET', '/sub-category/bootgrid', compact('start', 'length', 'search', 'order'));
-    }
+	public function bootgrid($start, $length, $search, array $order)
+	{
+		return $this->request('GET', '/sub-category/bootgrid', compact('start', 'length', 'search', 'order'));
+	}
 
 	/**
 	 * @param $id
 	 * @param array $data contains attribute_id
 	 * @return array|mixed|object
 	 */
-    public function attributes($id, array $data)
-    {
-        return $this->request('POST', '/sub-category/' . $id . '/attributes', $data);
-    }
+	public function attributes($id, array $data)
+	{
+		return $this->request('POST', '/sub-category/' . $id . '/attributes', $data);
+	}
 
-    public function store(SubCategoryDomain $subCategory)
-    {
-    	$response = $this->request('POST', '/sub-category', $subCategory->toArray());
-        return new SubCategoryDomain($response);
-    }
+	public function store(SubCategoryDomain $subCategory)
+	{
+		$response = $this->request('POST', '/sub-category', $subCategory->toArray());
 
-    public function update(SubCategoryDomain $subCategory)
-    {
-        $response = $this->request('PUT', '/sub-category/' . $subCategory->getId(), $subCategory->toArray());
-	    return new SubCategoryDomain($response);
-    }
+		return new SubCategoryDomain($response);
+	}
 
-    public function publish($id)
-    {
-        return $this->request('POST', '/sub-category/' . $id . '/publish', []);
-    }
+	public function update(SubCategoryDomain $subCategory)
+	{
+		$response = $this->request('PUT', '/sub-category/' . $subCategory->getId(), $subCategory->toArray());
 
-    public function unpublish($id)
-    {
-        return $this->request('POST', '/sub-category/' . $id . '/unpublish', []);
-    }
+		return new SubCategoryDomain($response);
+	}
+
+	public function publish($id)
+	{
+		return $this->request('POST', '/sub-category/' . $id . '/publish', []);
+	}
+
+	public function unpublish($id)
+	{
+		return $this->request('POST', '/sub-category/' . $id . '/unpublish', []);
+	}
 }
